5a58ca80f14206f4acedf44abefff7bd23b3dbef
AnselChien <anselchein1018@gmail.com>
2023-01-31 18:02:31 +0800

    Add project files.

---
+++ HomeWork/Models/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeWork.Models
+{
+    public class Class
+    {
+        public string ClassNo { get; set; }
+        public string ClassName { get; set; }
+        public int Credit { get; set; }
+        public string ClassRoom { get; set; }
+        public string Lecturer { get; set; }
+    }
+}
---
+++ HomeWork/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeWork.Models
+{
+    public class Student
+    {
+        public string Student_Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+
+    }
+}
