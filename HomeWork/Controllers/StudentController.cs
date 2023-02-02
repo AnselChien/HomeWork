23b34bb3a79dde812f3da9ca6e004d99e0337452
AnselChien <anselchein1018@gmail.com>
2023-02-02 16:19:08 +0800

    feat: adjust project's structure and rename items

--- HomeWork/Controllers/StudentController.cs
+++ HomeWork/Controllers/StudentController.cs
@@ -1,45 +1,138 @@
-﻿using HomeWork.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using HomeWork.Models;
 
 namespace HomeWork.Controllers
 {
-    public class StudentController : Controller
+    public class StudentController : System.Web.Mvc.Controller
     {
+        private HomeworkEntities db = new HomeworkEntities();
+
+        [HttpGet]
         public ActionResult Index()
         {
-            List<Student> students = new List<Student>();
+            ViewData["Students"] = db.Students.ToList();
+            return View();
+            //return View(db.Students.ToList());
+        }
 
-            using (HomeworkEntities conn = new HomeworkEntities())
+        // GET: test/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
             {
-                students = (from s in conn.Students select s).ToList();
-                ViewData["Students"] = conn.Students.ToList();
-                return View();
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: test/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index([Bind(Include = "學號,姓名,生日,Email")] Student student)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                try
+                {
+                    await db.SaveChangesAsync();
+
+                }
+                catch (DbUpdateConcurrencyException e)
+                {
+                    string ErrorMsg = e.Message;
+                }
+
+                return RedirectToAction("Index");
             }
 
+
+            return View(student);
         }
 
-        public ActionResult addStudent()
+        // GET: test/Edit/5
+        public ActionResult Edit(string id)
         {
-            return View();
-        }
-
-        [HttpPost] 
-        ActionResult addStudent(Student postback)
-        {
-            using (HomeworkEntities conn = new HomeworkEntities())
+            if (id == null)
             {
-                conn.Students.Add(postback);
-
-                conn.SaveChanges();
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-
-            return View();
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewData["Students"] = db.Students.ToList();
+            return View(student);
         }
 
-      
+        // POST: test/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "學號,姓名,生日,Email")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: test/Delete/5
+        //public ActionResult Delete(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Student student = db.Students.Find(id);
+        //    if (student == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(student);
+        //}
+
+        // POST: test/Delete/5
+        [HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Delete(string id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
