09af20ab0516dac3e0ec03243ab4031a7bdc7c3b
AnselChien <anselchein1018@gmail.com>
2023-02-03 15:51:59 +0800

    feat: create class function finished

---
+++ HomeWork/Controllers/ClassController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HomeWork.Models;
+
+namespace HomeWork.Controllers
+{
+    public class ClassController : Controller
+    {
+        private HomeworkEntities db = new HomeworkEntities();
+
+        // GET: Class
+        public ActionResult Index()
+        {
+            ViewData["Classes"] = db.Classes.ToList();
+            return View();
+        }
+
+        // GET: Class/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class @class = db.Classes.Find(id);
+            if (@class == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@class);
+        }
+
+       
+
+        // POST: Class/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "課號,課名,學分數,上課地點,講師名字")] Class @class)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Classes.Add(@class);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(@class);
+        }
+
+        // GET: Class/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class @class = db.Classes.Find(id);
+            if (@class == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@class);
+        }
+
+        // POST: Class/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "課號,課名,學分數,上課地點,講師名字")] Class @class)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(@class).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(@class);
+        }
+
+        // GET: Class/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class @class = db.Classes.Find(id);
+            if (@class == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@class);
+        }
+
+        // POST: Class/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Class @class = db.Classes.Find(id);
+            db.Classes.Remove(@class);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
